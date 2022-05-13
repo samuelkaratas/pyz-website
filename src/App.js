a134fa83ce487b8a818b626643b6ea9e5f3517e2
samuelkaratas <samuelkaratas@gmail.com>
2022-05-13 11:20:17 +0300

    mvp wishlist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+import { joinWishlist } from './firebase';
+
 function App() {
+
+  const [email, setEmail] = useState('')
+  const [sent, setSent] = useState(false)
+
+  const onClickHandler = () => {
+    if (!email) return
+    joinWishlist(email).then(res => {
+      if (res) {
+        setEmail('')
+        setSent(true)
+        // alert("Thanks for joining the wishlist! We'll notify you as soon as we launch. Cheers 🍻")
+      }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hero-image">
+      <div className="hero-text">
+        <h1 style={{ fontSize: "40px", color: '#94f7ff', fontFamily: 'Quantum' }}>COMING SOON!</h1>
+        {!sent ? <div className="input-group mb-3">
+          <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" className="form-control" placeholder="Email" aria-label="Email" aria-describedby="button-addon2" style={{ color: 'white', borderWidth: '1px', borderColor: '#94f7ff', backgroundColor: '#b957ce88' }} />
+          <button
+            onClick={onClickHandler}
+            className="btn btn-outline btn-change7"
+            type="button" id="button-addon2"
+            style={{ borderWidth: '1px', borderColor: '#94f7ff', color: '#94f7ff', backgroundColor: '#b957ce88', fontWeight: 'bold' }}
+          >Notify Me!</button>
+        </div> : <h3 style={{ fontSize: "40px", color: '#94f7ff', fontFamily: 'Quantum' }}>Thanks for joining the wishlist! We'll notify you as soon as we launch. Cheers 🍻</h3>}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+//<TextField id="outlined-basic" label="E-mail" variant="outlined" />
---
+++ src/firebase.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+import { getFirestore, addDoc, collection } from "firebase/firestore";
+require("firebase/firestore");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyArj58c85gaZPin9Sn5t32Ef4CXa66IaG0",
+    authDomain: "pyz-web.firebaseapp.com",
+    projectId: "pyz-web",
+    storageBucket: "pyz-web.appspot.com",
+    messagingSenderId: "740709873027",
+    appId: "1:740709873027:web:e702b876abc66be3d7f2b6",
+    measurementId: "G-6DEQMG3RPG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getFirestore(app);
+
+export const joinWishlist = async (email) => {
+    const docRef = await addDoc(collection(db, "wishlist"), {
+        email
+    });
+    return docRef.id
+}
