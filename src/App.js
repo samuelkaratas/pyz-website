f1d4321176c13113384ef6d6e33d5051e4db6a92
samuelkaratas <samuelkaratas@gmail.com>
2022-05-15 20:57:03 +0300

    adding emailjs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 
 import { joinWishlist } from './firebase';
 
+import { AiFillInstagram, AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai';
+
+//user_lJSn9FZGhGrtXm0F8A0jw"
+
+import emailjs from '@emailjs/browser';
+
 function App() {
 
   const [email, setEmail] = useState('')
@@ -12,6 +18,12 @@ function App() {
     if (!email) return
     joinWishlist(email).then(res => {
       if (res) {
+        emailjs.send('service_jw94g4r', 'template_10m6qyu', {email: email}, 'user_lJSn9FZGhGrtXm0F8A0jw')
+          .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+          }, function (error) {
+            console.log('FAILED...', error);
+          });
         setEmail('')
         setSent(true)
         // alert("Thanks for joining the wishlist! We'll notify you as soon as we launch. Cheers 🍻")
@@ -31,7 +43,13 @@ function App() {
             type="button" id="button-addon2"
             style={{ borderWidth: '1px', borderColor: '#94f7ff', color: '#94f7ff', backgroundColor: '#b957ce88', fontWeight: 'bold' }}
           >Notify Me!</button>
-        </div> : <h3 style={{ fontSize: "40px", color: '#94f7ff', fontFamily: 'Quantum' }}>Thanks for joining the wishlist! We'll notify you as soon as we launch. Cheers 🍻</h3>}
+        </div> : <h3 style={{ fontSize: "30px", color: '#94f7ff', fontFamily: 'Quantum' }}>Thanks for joining the wishlist! We'll notify you as soon as we launch. Cheers 🍻</h3>}
+        <div style={{ display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'space-around' }}>
+          <a href="https://instagram.com/pyzstudio/" target="_blank" rel="noreferrer"><h3><AiFillInstagram color='#94f7ff' /></h3></a>
+          <a href="https://twitter.com/pyzstudio/" target="_blank" rel="noreferrer"><h3><AiFillTwitterCircle color='#94f7ff' /></h3></a>
+          <a href="https://www.linkedin.com/company/pyz-realise-design-studio/" target="_blank" rel="noreferrer"><h3><AiFillLinkedin color='#94f7ff' /></h3></a>
+        </div>
+
       </div>
     </div>
   );
